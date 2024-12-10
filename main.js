d5eea77ba4b72128348e18e7a117be314d651957
Christian Danner <christian.danner@hotmail.com>
2024-12-10 18:41:07 -0300

    criação da função do botão no JS, e link com o HTML

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+/*
+||==============================================||
+|| Script em JS para sistema WEB                ||
+||                                              ||
+|| Autor: Christian Danner Ramos de Carvalho    ||
+||==============================================||
+*/
+
 function adcNovaTarefa() {
-    const list = document.getElementById("lista")
+    var lista = document.getElementById("lista") // pegar o conteúdo da lista gerada dentro da variável lista
+    var texto = document.getElementById("nome_tarefa").value; // pegar a entrada de texto do usuário
+    var itemLista = document.createElement("li"); // criar na tela o li, colocar o texto dentro da minha lista de itens, e colocar a lista de itens dentro da lista
+    const elementoTexto = document.createTextNode(texto);
+    itemLista.appendChild(elementoTexto);
+    lista.appendChild(itemLista);
 }
