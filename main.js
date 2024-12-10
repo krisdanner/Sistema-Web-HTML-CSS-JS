1f2d462fcee5d30da02d8dad6477e60cc46bec40
Christian Danner <christian.danner@hotmail.com>
2024-12-10 18:47:00 -0300

    correção nome da função no HTML

--- main.js
+++ main.js
@@ -7,9 +7,11 @@
 */
 
 function adcNovaTarefa() {
-    var lista = document.getElementById("lista") // pegar o conteúdo da lista gerada dentro da variável lista
+    var lista = document.getElementById("lista"); // pegar o conteúdo da lista gerada dentro da variável lista
     var texto = document.getElementById("nome_tarefa").value; // pegar a entrada de texto do usuário
     var itemLista = document.createElement("li"); // criar na tela o li, colocar o texto dentro da minha lista de itens, e colocar a lista de itens dentro da lista
+    itemLista.className = "lista-itens";
+    
     const elementoTexto = document.createTextNode(texto);
     itemLista.appendChild(elementoTexto);
     lista.appendChild(itemLista);
